cf61cac45b0242cc168cfb5dec04613731c5cee0
wingsun <wingsun@vgt-hk.net>
2023-07-13 17:38:56 +0800

    init code

---
+++ lib/data/dummy_items.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_shopping_list/models/grocery_item.dart';
+import 'package:flutter_shopping_list/data/categories.dart';
+import 'package:flutter_shopping_list/models/category.dart';
+
+final groceryItems = [
+  GroceryItem(
+    id: 'a',
+    name: 'Milk',
+    quantity: 1,
+    category: categories[Categories.dairy]!,
+  ),
+  GroceryItem(
+    id: 'b',
+    name: 'Bananas',
+    quantity: 5,
+    category: categories[Categories.fruit]!,
+  ),
+  GroceryItem(
+    id: 'c',
+    name: 'Beef Steak',
+    quantity: 1,
+    category: categories[Categories.meat]!,
+  ),
+];
